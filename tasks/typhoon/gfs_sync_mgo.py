7fa70c17acad14fc325e3eea1201055053efe9eb
hamanwendy <hamanwendy@outlook.com>
2022-05-13 10:10:55 +0800

    更新 gfs 同步脚本

---
+++ tasks/typhoon/gfs_sync_mgo.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os,sys
+import logging
+import datetime
+import pymongo
+from pandas.errors import EmptyDataError
+import pandas as pd
+from basic.database import get_mgo, MgoStore
+from tasks.typhoon.deps import HandleTyphoon
+import subprocess
+INPUT_PATH = os.getenv('INPUT_PATH', "/Users/jiufangkeji/Documents/JiufangCodes/jiufang-ls-tasks/typhoon_data/UNIFORM/")
+
+
+class GfsSyncMgo:
+    def __init__(self):
+        self.HISTORY_YEAR = os.getenv('HISTORY_YEAR', "2021")  
+        mgo_client, mgo_db = get_mgo()
+        config = {
+            "mgo_client": mgo_client,
+            "mgo_db": mgo_db,
+            'collection': 'gfs_data',
+            'uniq_idx': [
+                ('forecast_time', pymongo.ASCENDING),
+                ('StormID', pymongo.ASCENDING),
+                ('Basin', pymongo.ASCENDING)
+            ],
+            'idx_dic': {
+                    'typhoon_idx': [
+                        ('forecast_time', pymongo.ASCENDING),
+                        ('typhoon_id', pymongo.ASCENDING)
+                    ],
+                }
+            }
+        self.mgo = MgoStore(config)  # 初始化
+
+    def close(self):
+        self.mgo.close()
+
+    def history(self):
+        try:
+            res = subprocess.getoutput(f"ls -a {INPUT_PATH} |grep gfs_{self.HISTORY_YEAR}")
+            list_gfs = res.split('\n')
+            for file in list_gfs:
+                if "swp" in file:
+                    continue
+                if "csv" not in file:
+                    continue
+                gfs_file = INPUT_PATH + file
+                print(gfs_file)
+                try:
+                    df = pd.read_csv(gfs_file)
+                except EmptyDataError as e:
+                    continue
+                for index, row in df.iterrows():
+                    handle_typhoon = HandleTyphoon(mgo=mgo,row=row)
+                    flag = handle_typhoon.query_gfs_typhoon()
+                    if flag:
+                        print('开始匹配数据...')
+                        typhoon_id = handle_typhoon.query_real_time_typhoon()
+                        handle_typhoon.save_gfs_data(typhoon_id)
+                        # break
+        except Exception as e:
+            logging.error('run error {}'.format(e))
+        finally:
+            self.close()
+         
+    def run(self):
+        try:
+            date_now = datetime.datetime.now().strftime("%Y%m%d")
+            print(f'当前启动任务，入库时间== {date_now} ==')
+            res = subprocess.getoutput(f"ls -a {INPUT_PATH} |grep gfs_{date_now}")
+            if res:
+                print(res)
+                list_gfs = res.split('\n')
+                for file in list_gfs:
+                    gfs_file = INPUT_PATH + file
+                    print(gfs_file)
+                    try:
+                        df = pd.read_csv(gfs_file)
+                    except EmptyDataError as e:
+                        pass  
+                    for index, row in df.iterrows():
+                        handle_typhoon = HandleTyphoon(mgo=mgo,row=row)
+                        flag = handle_typhoon.query_gfs_typhoon()
+                        if flag:
+                            typhoon_id = handle_typhoon.query_real_time_typhoon()
+                            handle_typhoon.save_gfs_data(typhoon_id)
+        except Exception as e:
+            logging.error('run error {}'.format(e))
+        finally:
+            self.close()
--- tasks/typhoon/task_dic.py
+++ tasks/typhoon/task_dic.py
@@ -4,6 +4,7 @@ import logging
 from tasks.typhoon.noaa_sync_mgo import NoaaSyncMgo
 from tasks.typhoon.ssec_sync_mgo import SsecSyncMgo
 from tasks.typhoon.typhoon_sync_mgo import TyphoonSyncMgo
+from tasks.typhoon.gfs_sync_mgo import GfsSyncMgo
 from basic.scheduler import CustomScheduler
 
 def get_task_dic():
@@ -12,5 +13,6 @@ def get_task_dic():
         "noaa_sync_mgo": lambda: CustomScheduler(NoaaSyncMgo).run(),
         "ssec_sync_mgo": lambda: CustomScheduler(SsecSyncMgo).run(),
         "typhoon_sync_mgo": lambda: CustomScheduler(TyphoonSyncMgo).run(),
+        "gfs_sync_mgo": lambda: CustomScheduler(GfsSyncMgo).run(),
     }
     return task_dict
