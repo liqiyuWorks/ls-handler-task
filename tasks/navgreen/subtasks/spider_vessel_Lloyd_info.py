8afd8609c75448eb19549d08d844fdd4fed228d3
liqiyu <hamanwendy@outlook.com>
2025-07-25 11:50:34 +0800

    Refactor SpiderVesselsLloydInfo class to enhance data processing by adding batch size and sleep time configurations. Improve IMO list filtering to exclude invalid values and update vessel information with timestamps during database operations.

--- tasks/navgreen/subtasks/spider_vessel_Lloyd_info.py
+++ tasks/navgreen/subtasks/spider_vessel_Lloyd_info.py
@@ -41,16 +41,16 @@ def request_voc_detail(token, imo_list):
     }
 
     payload = {"lrnoList": imo_list[1:], "mmsiList": []}
-    print(payload)
     response = requests.request("POST", url, json=payload, headers=headers)
-    print(response.text)
     return response.json().get("data", [])
 
 
 class SpiderVesselsLloydInfo(BaseModel):
     def __init__(self):
+        self.time_sleep_seconds = float(os.getenv('TIME_SLEEP_SECONDS', 10))
         # "客船,干散货,杂货船,液体散货,特种船,集装箱"]
-        self.vessel_types = os.getenv('VESSEL_TYPES', "干散货")
+        self.batch_size = int(os.getenv('BATCH_SIZE', 10))
+        self.vessel_types = os.getenv('VESSEL_TYPES', "杂货船")
         if self.vessel_types:
             self.vessel_types = self.vessel_types.split(",")
         else:
@@ -69,38 +69,52 @@ class SpiderVesselsLloydInfo(BaseModel):
 
     @decorate.exception_capture_close_datebase
     def run(self):
-        # token = get_check_svc_token(self.cache_rds)
         try:
             dataTime = datetime.datetime.now().strftime("%Y-%m-%d %H:00:00")
             print(dataTime)
             imo_list = self.mgo_db["hifleet_vessels"].find({
-                "imo": {"$exists": True, "$ne": "None", "$ne": "0"},
-                "vesselTypeNameCn": {"$in": self.vessel_types }
+                "imo": {
+                    "$exists": True, 
+                    "$ne": "None", 
+                    "$ne": "0",
+                    "$ne": None,
+                    "$ne": ""
+                    },
+                "vesselTypeNameCn": {"$in": self.vessel_types}
             }).distinct("imo")
 
             # 获取已存在的lrno列表
             existing_lrno_list = self.mgo_db["Lloyd_info_vessels"].distinct(
                 "lrno")
             # imo_list与已存在的lrno去重
+            INVALID_IMO_VALUES = {None, "", "None", "0"}
             imo_list_after_deduplication = [
-                imo for imo in imo_list if imo not in existing_lrno_list]
-            print(len(imo_list), len(existing_lrno_list),
-                  len(imo_list_after_deduplication))
+                imo for imo in imo_list
+                if imo not in existing_lrno_list and imo not in INVALID_IMO_VALUES
+            ]
+            print(len(imo_list),len(imo_list_after_deduplication))
+            # print(imo_list_after_deduplication)
 
             # imo_list按照 1000 个一个数组，拆分循环
-            batch_size = 1000
+            batch_size = self.batch_size
             token = get_check_voc_token(self.cache_rds)
             imo_batches = [imo_list_after_deduplication[i:i + batch_size]
                            for i in range(0, len(imo_list_after_deduplication), batch_size)]
             for batch in imo_batches:
                 print(f"Processing batch of {len(batch)} IMOs")
-                print(batch)
                 res = request_voc_detail(token, batch)
                 for item in res:
-                    print(item.get("fmShipDTO").get("lrno"))
+                    print(item.get("fmShipDTO").get("lrno"), " -- 插入完成")
                     # 如果以imo为主键，存在则更新，不存在则插入（upsert=True已实现此逻辑）
+                    # 插入更新时间
+                    item["update_time"] = datetime.datetime.now()
                     self.mgo_db["Lloyd_info_vessels"].update_one(
                         {"lrno": item.get("fmShipDTO").get("lrno")}, {"$set": item}, upsert=True)
+                    
+                time.sleep(self.time_sleep_seconds)
+                    
+                # break
 
         except Exception as e:
+            traceback.print_exc()
             print("error:", e)
