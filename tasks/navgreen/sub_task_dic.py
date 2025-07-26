b60b4e91a5d920080d359d941510d215d6ab4e41
liqiyu <hamanwendy@outlook.com>
2025-07-27 00:45:10 +0800

    Update sub_task_dic.py to include RichHifleetVesselsInfo task and refine existing task descriptions for clarity.

--- tasks/navgreen/sub_task_dic.py
+++ tasks/navgreen/sub_task_dic.py
@@ -7,9 +7,12 @@ from tasks.navgreen.subtasks.spider_windy_zoom_storms import SpiderWindyZoomStor
 from tasks.navgreen.subtasks.calc_vessel_performance_details_from_wmy import CalcVesselPerformanceDetailsFromWmy
 from tasks.navgreen.subtasks.spider_vessel_Lloyd_info import SpiderVesselsLloydInfo
 from tasks.navgreen.subtasks.spider_vessel_finder_vessels import SpiderVesselFinderVessels
+from tasks.navgreen.subtasks.rich_hifleet_vessels_info import RichHifleetVesselsInfo
+
 def get_task_dic():
     task_dict = {
-        "spider_hifleet_vessels": (lambda: SpiderHifleetVessels(), 'Navgreen => hifleet的船舶档案'),
+        "spider_hifleet_vessels": (lambda: SpiderHifleetVessels(), 'Navgreen => 1、hifleet的船舶列表'),
+        "rich_hifleet_vessels_info": (lambda: RichHifleetVesselsInfo(), 'Navgreen => 2、丰富hifleet的船舶详情档案'),
         # "spider_vessel_finder_vessels": (lambda: SpiderVesselFinderVessels(), 'Navgreen => vesselfinder的列表'),
         "spider_vessel_Lloyd_info": (lambda: SpiderVesselsLloydInfo(), 'Navgreen => ***查询船舶劳氏船级社的档案'),
         "calc_vessel_performance_details": (lambda: CalcVesselPerformanceDetails(), '****Navgreen：后台计算船舶性能详情'),
