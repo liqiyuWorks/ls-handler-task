b7926cb9bea115930ad56ff18a306db7dd2ccbd8
liqiyu <hamanwendy@outlook.com>
2025-12-13 10:24:33 +0800

    fix: update vessel type filter in SyncGlobalVesselsFuelFromAxs and modify task dictionary
    - Updated the MongoDB query in SyncGlobalVesselsFuelFromAxs to include multiple vessel types: "杂货船", "干散货", and an empty string for improved data retrieval.
    - Commented out the ModifyVesselsInfoInMgo task in sub_task_dic.py to prevent its execution.

--- tasks/navgreen/sub_task_dic.py
+++ tasks/navgreen/sub_task_dic.py
@@ -14,7 +14,7 @@ def get_task_dic():
         "spider_hifleet_vessels": (lambda: SpiderHifleetVessels(), 'Navgreen => 1、hifleet的船舶列表'),
         
         "rich_hifleet_vessels_info": (lambda: RichHifleetVesselsInfo(), 'Navgreen => 2、丰富hifleet的船舶详情档案'),
-        "modify_vessels_info_in_mgo": (lambda: ModifyVesselsInfoInMgo(), 'Navgreen => 3、修改mgo中的船舶详情档案'),
+        # "modify_vessels_info_in_mgo": (lambda: ModifyVesselsInfoInMgo(), 'Navgreen => 3、修改mgo中的船舶详情档案'),
         
         "spider_vessel_Lloyd_info": (lambda: SpiderVesselsLloydInfo(), 'Navgreen => ***查询船舶劳氏船级社的档案'),
         "calc_vessel_performance_details_from_wmy": (lambda: CalcVesselPerformanceDetailsFromWmy(), '****Navgreen：从茂源那边获取 mmsi 去计算船舶性能详情'),
